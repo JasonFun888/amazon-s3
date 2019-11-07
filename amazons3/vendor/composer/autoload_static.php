4b1dfcf1150c0da0718f50d004baeb7a1760a66b
Jason <jasonfun888@gmail.com>
2019-11-07 15:23:01 +0800

    sync from SD-APP-Web-FK-Prod01: vendor...? modified:   amazons3/vendor/aws/aws-sdk-php/src/ modified:   amazons3/vendor/composer/ modified:   amazons3/vendor/guzzlehttp

--- amazons3/vendor/composer/autoload_static.php
+++ amazons3/vendor/composer/autoload_static.php
@@ -7,6 +7,7 @@ namespace Composer\Autoload;
 class ComposerStaticInita9c12d4e27e840c3cbc67e993d564c84
 {
     public static $files = array (
+        '7b11c4dc42b3b3023073cb14e519683c' => __DIR__ . '/..' . '/ralouphie/getallheaders/src/getallheaders.php',
         'c964ee0ededf28c96ebd9db5099ef910' => __DIR__ . '/..' . '/guzzlehttp/promises/src/functions_include.php',
         'a0edc8309cc5e1d60e3047b5df6b7052' => __DIR__ . '/..' . '/guzzlehttp/psr7/src/functions_include.php',
         'b067bc7112e384b61c701452d53a14a8' => __DIR__ . '/..' . '/mtdowling/jmespath.php/src/JmesPath.php',
